9c7fbc0146d0e7d9e7ab3f74bbe96494967bdfe8
psawle <psawle4@gmail.com>
2020-10-10 01:10:24 +0530

    updated our menu-items with react-router-dom 0h HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page </h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,39 +1,11 @@
 import React from "react";
 
 import "./menu-item.styles.scss";
+import {withRouter} from "react-router-dom"
 
-// const MenuItem = ({ title, imageUrl, size }) => (
-//   <div className={`${size} menu-item`}>
-//     <div
-//       className="background-image"
-//       style={{
-//         backgroundImage: `url(${imageUrl})`,
-//       }}
-//     />
-//     <div className="content">
-//       <h1 className="title">{title.toUpperCase()}</h1>
-//       <span className="subtitle">SHOP NOW</span>
-//     </div>
-//   </div>
-// );
-
-// export default MenuItem;
-
-// import React, { Component } from "react";
-
-// export default class MenuItem extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>ab to chal jana chahiye</h1>
-//       </div>
-//     );
-//   }
-// }
-
-function MenuItem({ title, imageUrl, size }) {
+function MenuItem({ title, imageUrl, size,history, linkUrl,match }) {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push((`${match.url}${linkUrl}`)) }  >
       <div
         className="background-image"
         style={{
@@ -48,4 +20,4 @@ function MenuItem({ title, imageUrl, size }) {
   );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
